b7d280d3364b869219abd3e106aa239025410328
starknguyen <ngdinhnghi@gmail.com>
2016-12-04 17:26:36 +0200

    Added new icon for application

--- LogFilterApplication/LogFilterApplication/MainForm.Designer.cs
+++ LogFilterApplication/LogFilterApplication/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.btnBrowseInputFile = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tbInputFileLocation = new System.Windows.Forms.TextBox();
@@ -156,6 +157,7 @@
             this.ClientSize = new System.Drawing.Size(616, 336);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Log Filter Application";
             this.groupBox1.ResumeLayout(false);
